37ef99f1fdc8959a0ecf74cbf406b0be7bcaf3f9
Dzmitry Viartseika <verteyko1990@gmail.com>
2024-05-03 22:56:44 +0200

    add moderation service and logic

--- moderation/index.js
+++ moderation/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
 
 app.post('/events', async (req, res) => {
-    const {type, data} = req.body;
+    const { type, data } = req.body;
 
     if (type === 'CommentCreated') {
         const status = data.content.includes('orange') ? 'rejected' : 'approved';
@@ -19,12 +17,14 @@ app.post('/events', async (req, res) => {
                 id: data.id,
                 postId: data.postId,
                 status,
-                content: data.content,
+                content: data.content
             }
-        })
+        });
     }
-})
+
+    res.send({});
+});
 
 app.listen(4003, () => {
-    console.log('Listener on 4003')
-})
+    console.log('Listening on 4003');
+});
--- query/index.js
+++ query/index.js
@@ -1,38 +1,50 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const posts = {}
+const posts = {};
 
 app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
 app.post('/events', (req, res) => {
-    const {type, data} = req.body;
+    const { type, data } = req.body;
 
     if (type === 'PostCreated') {
-        const {id, title} = data;
+        const { id, title } = data;
 
-        posts[id] = {id, title, comments: []}
+        posts[id] = { id, title, comments: [] };
     }
 
     if (type === 'CommentCreated') {
-        const {id, content, postId} = data;
+        const { id, content, postId, status } = data;
 
         const post = posts[postId];
-        post.comments.push({
-            id, content, status,
-        })
+        post.comments.push({ id, content, status });
     }
 
-    res.send({})
-})
+    if (type === 'CommentUpdated') {
+        const { id, content, postId, status } = data;
 
-app.listen((4002), () => {
-    console.log('Listening on 4002')
-})
+        const post = posts[postId];
+        const comment = post.comments.find(comment => {
+            return comment.id === id;
+        });
+
+        comment.status = status;
+        comment.content = content;
+    }
+
+    console.log(posts);
+
+    res.send({});
+});
+
+app.listen(4002, () => {
+    console.log('Listening on 4002');
+});
