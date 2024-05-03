b1840e7b01ebab58eae7098cbd1e392b0c68d680
Dzmitry Viartseika <verteyko1990@gmail.com>
2024-05-03 19:50:03 +0200

    add PostList.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,14 @@
-import PostCreate from "./components/Post/PostCreate";
+import PostCreate from "./components/Posts/PostCreate";
+import PostList from "./components/Posts/PostList";
 
 function App() {
   return (
       <div className="container">
           <h1>Create Post</h1>
           <PostCreate/>
+          <hr/>
+          <h1>Posts</h1>
+          <PostList/>
       </div>
   );
 }
